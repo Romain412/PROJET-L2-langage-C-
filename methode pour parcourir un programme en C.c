df89c5adde5ca7b1cfb697437eb25e1cca3de268
Romain412 <romainthomas786@gmail.com>
2022-12-07 14:53:38 +0100

    Update methode pour parcourir un programme en C.c

--- methode pour parcourir un programme en C.c
+++ methode pour parcourir un programme en C.c
@@ -1,27 +1,40 @@
+
+
+
 #include <stdio.h>
 
-int taille_fichier(FILE f){
-	char l = fgetc(fichier);
+
+int taille_fichier(FILE *f){ //RETOURNE LE NOMBRE DE CARACTERES DANS UN FICHIER
+	char lettre = fgetc(f);
 	int nb=1;
-	
-	while( l != EOF) {
-		l = fgetc(fichier);
+	while( lettre != EOF) {
+		lettre = fgetc(f);
 		nb++;
 	}
+	fseek(f,0,SEEK_SET);//Deplacement dans f de 0 caracteres a partir du debut (pour pouvoir parcourir le fichier plusieurs fois)
 	return nb;
 }
+
 
 int main(int argc, char **argv)
 {
     FILE *fichier=NULL;//creation du pointeur de fichier
     fichier=fopen("test2.c","r");//ouverture du fichier dans lequel on lit
 
-    //on affiche les 40 premiers caracteres du fichier
-    for(int i=0;i<40;i++){
+    //Remplissage d'un tableau contenant tous les caracteres du fichier qu'on est en train de lire
+    char texte[taille_fichier(fichier)];
     char lettre=fgetc(fichier);
-    printf("%c",lettre);
+    int i=0;
+    while (lettre!=EOF){
+        texte[i]=lettre;
+        lettre=fgetc(fichier);
+        printf("%c",texte[i]);
+        i++;
     }
-    fclose(fichier);//fermeture du fichier
+    fclose(fichier);
 	return 0;
+
+
+
 }
 
