baa0096458719e1f60a147b934900bf05c3a4fee
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2023-01-03 16:57:36 +0100

    Update Projet KNN code final.c

--- Projet KNN code final.c
+++ Projet KNN code final.c
@@ -17,7 +17,7 @@ struct vecteur{
 
 
 
-// calcule le nombre de lignes d'un CSV et enleve 1 (pour ne pas compter l'en-tête avec les noms de colonne)
+// Calcule le nombre de lignes d'un CSV et enleve 1 (pour ne pas compter l'en-tÃªte avec les noms de colonne)
 int nb_lignes_fichier_csv(FILE *f){
 	fseek(f,0,SEEK_SET);
 	char lettre = fgetc(f);
@@ -28,13 +28,13 @@ int nb_lignes_fichier_csv(FILE *f){
 		if(lettre == '\n') nb++;
 	}
 
-	fseek(f,0,SEEK_SET);	//Deplacement dans f de 0 caracteres a partir du debut (remet le fichier au debut)
+	fseek(f,0,SEEK_SET);	// Deplacement dans f de 0 caracteres a partir du debut (remet le curseur au debut du fichier)
 	return nb - 1;
 }
 
 
 
-//donne le nombre de colonnes
+// Donne le nombre de colonnes
 int nb_colonnes_fichier_csv(FILE *f){
     fseek(f,0,SEEK_SET);
     char lettre = fgetc(f);
@@ -51,27 +51,27 @@ int nb_colonnes_fichier_csv(FILE *f){
 
 
 
-// remplit un tableau de vecteurs avec les données d'un fichier CSV
+// Remplit un tableau de vecteurs avec les donnÃ©es d'un fichier CSV
 void remplir_tableau(FILE *doc, int taille, vecteur *tab){
 	int i=0;
 
-	char line[longueur_max_ligne];					//tableau qui stockera chaque ligne en chaîne de caractère
-	char *nb_temp;											//variable qui gardera les char entre chaque ";" du CSV
+	char line[longueur_max_ligne];					// Tableau qui stockera chaque ligne en chaÃ®ne de caractÃ¨re
+	char *nb_temp;							// Variable qui gardera les char entre chaque ";" du CSV
 
-	while(fgetc(doc) != '\n') fgetc(doc); 			//saute la ligne d'en-têtes
+	while(fgetc(doc) != '\n') fgetc(doc); 			// Saute la ligne d'en-tÃªtes
 
-	while (fgets(line, longueur_max_ligne, doc) != NULL){ 	//tant que la ligne n'est pas vide
+	while (fgets(line, longueur_max_ligne, doc) != NULL){ 	// Tant que la ligne n'est pas vide
 
-		nb_temp = strtok(line, ";");						// stocke dans nb_temp les caractère jusqu'au prochain caractère séparateur
-		tab[i].donnees[0] = atof(nb_temp);						//transforme la chaine de caractère en int et la stocke dans le tableau tab[i].donnees;
+		nb_temp = strtok(line, ";");				// Stocke dans nb_temp les caractÃ¨re jusqu'au prochain caractÃ¨re sÃ©parateur
+		tab[i].donnees[0] = atof(nb_temp);			// Transforme la chaine de caractÃ¨re en int et la stocke dans le tableau tab[i].donnees;
 
-        for(int z = 1;z < taille-1; z++){     //stocke le reste de la ligne dans tab[i].donnees
+        for(int z = 1;z < taille-1; z++){     // Stocke le reste de la ligne dans tab[i].donnees
           nb_temp = strtok(NULL, ";");
           tab[i].donnees[z] = atof(nb_temp);
         }
 
         nb_temp = strtok(NULL,";");
-        tab[i].categorie =atoi(nb_temp);        //stocke la derniere valeur de la ligne dans tab[i].categorie
+        tab[i].categorie =atoi(nb_temp);        // Stocke la derniere valeur de la ligne dans tab[i].categorie
 
         i++;
 	}
@@ -89,14 +89,14 @@ vecteur *creation_vecteur(int taille_vec){
 }
 
 
-//cree un tableau de vecteurs
+// Cree un tableau de vecteurs
 void *creation_tab_vec(vecteur *tab_vec,int nb_lignes,int taille_vec){
     for(int i=0;i<nb_lignes;i++){
         tab_vec[i]=*creation_vecteur(taille_vec-1);
     }
 }
 
-//calcule la distance euclidienne entre 2 vecteurs
+// Calcule la distance euclidienne entre 2 vecteurs
 float distance(vecteur vec1,vecteur vec2){
     float dist=0,tmp;
     for (int i=0;i<vec1.taille;i++){
@@ -108,7 +108,7 @@ float distance(vecteur vec1,vecteur vec2){
 }
 
 
-//retourne 1 si le vecteur est dans le cercle, 0 sinon
+// Retourne 1 si le vecteur est dans le cercle, 0 sinon
 int dans_cercle(vecteur vec,vecteur Point,int K){
 	if (distance(Point,vec)<=K) return 1;
 	return 0;
@@ -116,7 +116,7 @@ int dans_cercle(vecteur vec,vecteur Point,int K){
 
 
 
-//remplit le tableau des vecteurs voisins et qui nous donne sa nouvelle taille
+// Remplit le tableau des vecteurs voisins et qui nous donne sa nouvelle taille
 void voisins(vecteur *tab_vois, vecteur *tab_vec, int taille_tab_vec, vecteur Point, int K,int *p_nv_taille){
     int j=0;
     for (int i=0;i<taille_tab_vec;i++){
@@ -132,18 +132,18 @@ void voisins(vecteur *tab_vois, vecteur *tab_vec, int taille_tab_vec, vecteur Po
 
 
 
-//compte le nombre de vecteurs par categorie et rempli le tableau associe
+// Compte le nombre de vecteurs par categorie et rempli le tableau associe
 void compteur_cate(vecteur *tab_vois, int *tab_categ, int taille){
 	vecteur vec;
 	for (int i=0;i<taille;i++){
 		vec=tab_vois[i];
-		tab_categ[vec.categorie]++;//+1 pour la categorie du vecteur selectionne a ce tour
+		tab_categ[vec.categorie]++;	// +1 pour la categorie du vecteur selectionne a ce tour
 	}						  // ex: si vec.cate=1 -> tab_categ[1]++,    si vec.cate=0 -> tab_categ[0]++
 }
 
 
 
-//retourne la categorie la plus presente dans le tableau des categories
+// Retourne la categorie la plus presente dans le tableau des categories
 int maxi(int tab_categ[], int taille){
     int cate_max=0;
     for(int i=0;i<taille;i++){
@@ -166,14 +166,14 @@ int main(){
 
 
 	FILE *fichier = NULL;
-	fichier = fopen("heart_failure_clinical_records_dataset.csv","r");//ouverture du fichier
+	fichier = fopen("heart_failure_clinical_records_dataset.csv","r");	// Ouverture du fichier
     int nb_colonnes = nb_colonnes_fichier_csv(fichier);
     int nb_lignes=nb_lignes_fichier_csv(fichier);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	vecteur *tableau=malloc(sizeof(vecteur)*nb_lignes-1);//allocation du tableau de vecteurs
+	vecteur *tableau=malloc(sizeof(vecteur)*nb_lignes-1);	// Allocation du tableau de vecteurs
 
     creation_tab_vec(tableau,nb_lignes,nb_colonnes);
     remplir_tableau(fichier,nb_colonnes,tableau);
@@ -181,17 +181,17 @@ int main(){
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    int donnees_point[]={18,0,150,1,60,0,250000,1,140,0,1,10};//Ce sont des donnees arbitraires, il est parfaitement possible de les remplacer par celles que vous voulez
-    vecteur Point={donnees_point,nb_colonnes-1,-1};//vecteur de categorie inconnue contenant les donnees de la ligne precedente
+    int donnees_point[]={18,0,150,1,60,0,250000,1,140,0,1,10};	// Ce sont des donnees arbitraires, il est parfaitement possible de les remplacer par celles que vous voulez
+    vecteur Point={donnees_point,nb_colonnes-1,-1};	// Vecteur de categorie inconnue contenant les donnees de la ligne precedente
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    vecteur *tab_vois=malloc(sizeof(vecteur)*nb_lignes);//tableau qui va contenir tous les vecteurs dans le cercle (voisins)
-    int nvlle_taille_vois=nb_lignes;//nouvelle taille du tableau des vecteurs voisins
+    vecteur *tab_vois=malloc(sizeof(vecteur)*nb_lignes);	// Tableau qui va contenir tous les vecteurs dans le cercle (voisins)
+    int nvlle_taille_vois=nb_lignes;	// Nouvelle taille du tableau des vecteurs voisins
     int *p_nv_taille=&nvlle_taille_vois;
 
-    int *tab_cate=calloc(nb_cate,sizeof(int));//tableau qui va contenir le nombre de vecteurs de chaque categorie
+    int *tab_cate=calloc(nb_cate,sizeof(int));	// Tableau qui va contenir le nombre de vecteurs de chaque categorie
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -208,7 +208,7 @@ int main(){
     scanf("%d",&menu);
 
     if (menu==1){
-        //affichage du tableau pour verifier
+        // Affichage du tableau pour verifier
         for(int y=0;y<nb_lignes-1;y++){
             printf("\n%d\t",y);
             for(int z=0;z<nb_colonnes-1;z++){
@@ -220,10 +220,10 @@ int main(){
     }
 
     if (menu==2){
-        //Affichage des voisins
-        for(int i=0;i<nvlle_taille_vois;i++){//pour chaque vecteur
+        // Affichage des voisins
+        for(int i=0;i<nvlle_taille_vois;i++){	// Pour chaque vecteur
                 printf("\n%d\t",i);
-            for(int j=0;j<nb_colonnes-1;j++){//pour chaque valeur dans ce vecteur
+            for(int j=0;j<nb_colonnes-1;j++){// Pour chaque valeur dans ce vecteur
                 printf("%f | ",tab_vois[i].donnees[j]);
             }
             printf("categorie: %d\t distance: %f ",tab_vois[i].categorie,distance(tab_vois[i],Point));
